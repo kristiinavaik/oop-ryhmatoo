e193c157b88313b67294e1deea7cea4af497dd9c
kristiinavaik <kristiina.vaik@hotmail.com>
2014-10-12 22:19:59 +0300

    Remove unused imports

--- src/Fraasid.java
+++ src/Fraasid.java
@@ -1,7 +1,5 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Random;
 import java.util.Scanner;
 
 public class Fraasid extends Andmed {
--- src/Laused.java
+++ src/Laused.java
@@ -1,7 +1,5 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Random;
 import java.util.Scanner;
 
 public class Laused extends Andmed {
